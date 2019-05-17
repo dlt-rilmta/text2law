049c36f5ff748328ea31340adc7926cadcfbccbf
David Halasz <david.peter.halasz@gmail.com>
2019-05-18 00:54:28 +0200

    Simplified argument parsing into function

--- text2law.py
+++ text2law.py
@@ -12,10 +12,10 @@ def read_file(finp):
         return text
 
 
-def write_out(list, finp, fname):
+def write_out(ls, finp, fname):
     Path(finp).mkdir(parents=True, exist_ok=True)
     with open(os.path.join(finp, fname), "w", encoding="utf-8") as f:
-        f.write("\n".join(list))
+        f.write("\n".join(ls))
 
 
 def remove_accent(s):
@@ -100,39 +100,41 @@ def extract_legislation(titles, splittext):
     return legislations
 
 
-def main():
+def get_args_inpfi_outfo(basp):
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath', help='Path to file', nargs="*")
     parser.add_argument('-d', '--directory', help='Path of output file(s)', nargs='?')
     args = parser.parse_args()
     files = []
-    basp = 'legislations'
     pat_slash = re.compile(r'\\|/')
 
     if args.filepath:
         for p in args.filepath:
             poss_files = glob(p)
-            if len(poss_files) > 1:
-                new_files = [os.path.join(*pat_slash.split(x)[0:-1], os.path.basename(x)) for x in poss_files]
-                files += new_files
-            else:
-                fname = os.path.basename(p)
-                raw_p = pat_slash.split(p.replace(fname, ""))
-                files.append(os.path.join(*raw_p, fname))
+            new_files = [os.path.join(*pat_slash.split(x)[0:-1], os.path.basename(x)) for x in poss_files]
+            files += new_files
+    else:
+        files = glob(os.path.join(os.getcwd(), "*html"))
 
     if args.directory:
         basp = os.path.join(*pat_slash.split(args.directory))
+    return basp, files
+
+
+def main():
+    basp = 'legislations'
+    basp, files = get_args_inpfi_outfo(basp)
 
     for finp in files:
         text = read_file(finp)
         titles = extract_titles(text)
         legislations = extract_legislation(titles, text.split("\n"))
-        # print(len(titles))
-        # print("\n".join(titles))
         for legislation in legislations:
-            pass
-            # leg_count.append(legislation)
-            write_out(legislation[2], os.path.join(basp, legislation[0]), legislation[1]+".txt")
+            prefix = ""
+            if legislation[0] in ["hatarozata", "torveny", "rendelete"]:  # teszt
+                prefix = {"hatarozata": "hat", "torveny": "trv", "rendelete": "rnd"}[legislation[0]]  # teszt
+
+            write_out(legislation[2], basp, prefix+"_"+legislation[1]+".txt")
 
 
 if __name__ == "__main__":
