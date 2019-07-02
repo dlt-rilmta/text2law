217becf8748aeb8de06b8030bc0978904d0ad436
David Halasz <david.peter.halasz@gmail.com>
2019-07-02 14:48:50 +0200

    can be run from terminal and can be used as import module

--- text2law.py
+++ text2law.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import argparse
 import os
 import re
@@ -6,18 +8,19 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 
 
-def read_file(finp):
-    with open(finp, encoding="utf-8") as f:
-        text = f.read()
-        return text
+def read(files):
+    for finp in files:
+        with open(finp, encoding="utf-8") as f:
+            fname = os.path.splitext(os.path.basename(finp))[0]
+            yield (fname, f.read())
 
 
-def write_out(ls, finp, fname, mk=None):
-    if mk is not None:
-        finp = os.path.join(finp, mk)
-    Path(finp).mkdir(parents=True, exist_ok=True)
-    with open(os.path.join(finp, fname), "w", encoding="utf-8") as f:
-        f.write("\n".join(ls))
+def write(outp, dir):
+    Path(dir).mkdir(parents=True, exist_ok=True)
+    for legislations in outp:
+        for legislation in legislations:
+            with open(os.path.join(dir, legislation[0]), "w", encoding="utf-8") as f:
+                f.write("\n".join(legislation[1]))
 
 
 def remove_accent(s):
@@ -90,22 +93,25 @@ def extract_titles(toc, prefix_dict=None):
     return titles
 
 
-def extract_legislation(titles, mk, bs_divs):
+
+def extract_legislation(titles, fname, bs_divs):
     pat_page_end = re.compile(r'.{,40}közlöny.{,40}')
     pat_non_chars = re.compile(r'\W')
+    pat_sign = re.compile(r's\.\s+k\.,')
+    pat_header = re.compile(r'(közlöny(?:\d+évi)?\d+szám)|(\d+szám\w+?közlöny)')
 
     legislation = []
     legislations = []
     is_legislation = False
+    after_signature = False
     leg_title = ""
-
     for div in bs_divs:
         page_end = "".join(pat_page_end.findall(pat_non_chars.sub("", div.text.lower())))
         for p in div.find_all('p'):
             p_cont = p.text.strip()
-            if p_cont == "":
-                continue
             raw_p = pat_non_chars.sub("", p_cont.lower())
+            if p_cont == "" or pat_header.search(raw_p):
+                continue
             next_p = p.findNext('p')
             if next_p:
                 next_p = "" or next_p.text
@@ -118,38 +124,18 @@ def extract_legislation(titles, mk, bs_divs):
                     is_legislation = True
                     if len(legislation) != 0:
                         legislations.append((leg_title, legislation))
-                    leg_title = remove_accent(title[1] + "_" + mk + "_" + pat_non_chars.sub(r'_', title[0])).lower()
+                    leg_title = remove_accent(title[1] + "_" + fname + "_" + pat_non_chars.sub(r'_', title[0])).lower()
                     legislation = []
+                    after_signature = False
                     del titles[i]
                     break
-
-            if is_legislation:
+            if pat_sign.search(p_cont) and not after_signature:
+                after_signature = True
+            if is_legislation and not after_signature:
                 legislation.append(p_cont)
     legislations.append((leg_title, legislation))
 
     return legislations
-
-
-def get_args_inpfi_outfo(basp):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', help='Path to file', nargs="*")
-    parser.add_argument('-d', '--directory', help='Path of output file(s)', nargs='?')
-
-    args = parser.parse_args()
-    files = []
-
-    if args.filepath:
-        for p in args.filepath:
-            poss_files = glob(p)
-            new_files = [os.path.abspath(x) for x in poss_files]
-            files += new_files
-    else:
-        files = glob(os.path.join(os.getcwd(), "*html"))
-
-    if args.directory:
-        basp = os.path.abspath(args.directory)
-
-    return basp, files
 
 
 def get_toc_and_cont(div_tags):
@@ -185,32 +171,56 @@ def get_toc_and_cont(div_tags):
     return divs_toc, divs
 
 
-def main():
-    basp = 'legislations'
-    basp, files = get_args_inpfi_outfo(basp)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='Path to file', nargs="+")
+    parser.add_argument('-d', '--directory', help='Path of output file(s)', nargs='?')
+    dir = 'legislations'
+    args = parser.parse_args()
+    files = []
 
+    if args.filepath:
+        for p in args.filepath:
+            poss_files = glob(p)
+            poss_files = [os.path.abspath(x) for x in poss_files]
+
+            files += poss_files
+    else:
+        files = glob(os.path.join(os.getcwd(), "*.html"))
+
+    if args.directory:
+        dir = os.path.abspath(args.directory)
+
+    return {'dir': dir, 'files': files}
+
+
+def process(inp):
     prefix_dict = {"határozat": "hat", "rendelet": "rnd", "törvény": "trv",
                    "végzés": "veg", "közlemény": "koz", "nyilatkozata": "nyil",
                    "utasítás": "ut", "állásfoglalás": "all", "hirdetmény": "hir",
                    "helyesbítés": "hely", "tájékoztató": "taj", "intézkedés": "int",
                    "parancs": "par", "pénzügyibeszámoló": "merl"}
-
-    for finp in files:
-        txt = read_file(finp)
+    for f in inp:
+        txt = f[1]
         soup = BeautifulSoup(txt, 'lxml')
         # extract table of contents and the content
         divs_toc, divs = get_toc_and_cont(soup.find_all('div'))
         p_parts_toc = [p.text for div in divs_toc for p in div]
         titles = extract_titles(p_parts_toc, prefix_dict)
-        print(finp)
-        # for title in titles:
-        #     print(title)
-
+		
+        # print(f[0])
+ 
         # extract legislations
-        legislations = extract_legislation(titles, os.path.splitext(finp.split("\\")[-1])[0], divs)
-        for legislation in legislations:
-            write_out(legislation[1], basp, legislation[0] + ".txt")
+        legislations = extract_legislation(titles, f[0], divs)
+        yield legislations
 
+
+def main():
+
+    args = get_args()
+    inp = read(args['files'])
+    outp = process(inp)
+    write(outp, args['dir'])
 
 
 if __name__ == "__main__":
